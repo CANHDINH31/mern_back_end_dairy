5fbeb836c20fc2228d84027b1615d967d8170a9d
PHAMCANHDINH <dinhphamcanh@gmail.com>
2022-03-05 00:20:13 +0700

    fix

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ const postRoute = require("./routes/postRoute");
 
 const { errorHandler } = require("./middlewares/errorsHandler");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -26,6 +31,16 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
 
+  next();
+});
+
+app.use(function (req, res, next) {
+  res.header("Content-Type", "application/json;charset=UTF-8");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
@@ -38,11 +53,7 @@ app.all("*", (req, res, next) => {
   next(err);
 });
 app.use(errorHandler);
-app.use(express.static(path.join(__dirname, "../front-end/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../front-end/build/index.html"));
-});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
